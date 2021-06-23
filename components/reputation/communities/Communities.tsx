6ce0d04bf3a0e58bd86d6030fd9f51e7d437a3bc
jinbest <jin.bestvictoria718@gmail.com>
2021-06-24 06:48:47 +0800

    reputation page completed with tab pages

---
+++ components/reputation/communities/Communities.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import config from "../../../static/config.json"
+import _ from "lodash"
+
+const Communities = () => {
+  const thisPage = _.cloneDeep(config.reputation.communities)
+
+  return (
+    <div className="communities-container">
+      <div className="partnered-bar">
+        <p>{thisPage.partnerBar.text}</p>
+        <a href={thisPage.partnerBar.link.href} target="_blank" rel="noreferrer">
+          {thisPage.partnerBar.link.name}
+        </a>
+      </div>
+      <div className="communities-data-container">
+        {thisPage.data.map((item: any, index: number) => {
+          return (
+            <a key={index} href={item.link} target="_blank" rel="noreferrer">
+              <img src={item.img_src} alt={`communities-logo-${index}`} />
+            </a>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Communities
---
+++ components/reputation/leader-board/LeaderBoard.tsx
@@ -0,0 +1,112 @@
+import React, { useState, useEffect, useCallback } from "react"
+import RepuLeaderBoardCard from "./RepuLeaderBoardCard"
+import CustomSearchBar from "../../CustomSearchBar"
+import { ReputationLeaderBoardCardParam } from "../../../models/reputation-params"
+import config from "../../../static/config.json"
+import _ from "lodash"
+
+const LeaderBoard = () => {
+  const thisPage = _.cloneDeep(config.reputation)
+  const repuFieldData = _.cloneDeep(
+    thisPage.repuLeaderBoard.fieldData
+  ) as ReputationLeaderBoardCardParam
+  const repuMainData = _.cloneDeep(
+    thisPage.repuLeaderBoard.childData
+  ) as ReputationLeaderBoardCardParam[]
+
+  const [repuFilter, setRepuFilter] = useState("allTime")
+  const [repuData, setRepuData] = useState<ReputationLeaderBoardCardParam[]>(repuMainData)
+  const [repuSearchKey, setRepuSearchKey] = useState("")
+
+  const handleRepuSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    setRepuSearchKey(e.target.value)
+  }
+
+  const handleRepuSearchIconClick = () => {
+    if (!repuSearchKey) {
+      setRepuData(repuMainData)
+    } else {
+      const cntRepuData = [] as ReputationLeaderBoardCardParam[]
+      for (let i = 0; i < repuMainData.length; i++) {
+        if (repuMainData[i].rank.toString().includes(repuSearchKey)) {
+          cntRepuData.push(repuMainData[i])
+        } else if (repuMainData[i].name.toLowerCase().includes(repuSearchKey.toLowerCase())) {
+          cntRepuData.push(repuMainData[i])
+        } else if (
+          repuMainData[i].discriminator.toLowerCase().includes(repuSearchKey.toLowerCase())
+        ) {
+          cntRepuData.push(repuMainData[i])
+        }
+      }
+      setRepuData(cntRepuData)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("keydown", onKeyPress, false)
+    return () => {
+      document.removeEventListener("keydown", onKeyPress, false)
+    }
+  })
+
+  const onKeyPress = useCallback(
+    (event) => {
+      if (event.key === "Enter") {
+        handleRepuSearchIconClick()
+      }
+    },
+    [repuSearchKey, repuData]
+  )
+
+  return (
+    <div className="reputation-leaderboard-container">
+      <RepuLeaderBoardCard data={repuFieldData} />
+      <div className="reputation-filter-container">
+        <div className="filter-search">
+          <CustomSearchBar
+            value={repuSearchKey}
+            placeholder="Search by DiscordID"
+            handleChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              handleRepuSearchChange(e)
+            }}
+            handleIconClick={handleRepuSearchIconClick}
+          />
+        </div>
+        <div className="filter-filter">
+          <p
+            onClick={() => {
+              setRepuFilter("daily")
+            }}
+            style={{ color: repuFilter === "daily" ? "white" : "" }}
+          >
+            Daily
+          </p>
+          <p
+            onClick={() => {
+              setRepuFilter("weekly")
+            }}
+            style={{ color: repuFilter === "weekly" ? "white" : "" }}
+          >
+            Weekly
+          </p>
+          <p
+            onClick={() => {
+              setRepuFilter("allTime")
+            }}
+            style={{ color: repuFilter === "allTime" ? "white" : "" }}
+          >
+            All-Time
+          </p>
+        </div>
+      </div>
+      <div className="reputation-main-data-container">
+        {repuData.map((item: ReputationLeaderBoardCardParam, index: number) => {
+          return <RepuLeaderBoardCard data={item} key={index} />
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default LeaderBoard
