cfe4efca3d7e4f6723d674cc57384bc6130e1ca3
jinbest <jin.bestvictoria718@gmail.com>
2021-07-03 05:26:16 +0800

    Sign with Google-OAuth, Discord & Steam account integration

--- config/config.ts
+++ config/config.ts
@@ -1,9 +1,23 @@
 const apiURL = "http://185.248.33.127:5000"
 export default class Config {
-  static REGISTER_API_URL = `${apiURL}/api/user/register`
-  static LOGIN_API_URL = `${apiURL}/api/user/login`
-  static GOOGLE_AUTH_LOGIN = `${apiURL}/api/user/googleAuth`
+  /* auth */
+  static REGISTER_API_URL = `${apiURL}/api/auth/register`
+  static LOGIN_API_URL = `${apiURL}/api/auth/login`
+  static GOOGLE_AUTH_SIGN_IN = `${apiURL}/api/auth/googleAuthSingIn`
+  static GOOGLE_AUTH_SIGN_UP = `${apiURL}/api/auth/googleAuthSignUp`
+  static STEAM_URL = `${apiURL}/api/auth/steam`
+  static DISCORD_URL = `${apiURL}/api/auth/discord`
+  static STEAM_SIGN_UP = `${apiURL}/api/auth/signUpWithSteamAccount`
+  static DISCORD_SIGN_UP = `${apiURL}/api/auth/signUpWithDiscordAccount`
+  static AUTH_TOKEN_TEST = `${apiURL}/api/auth/authTokenTest`
+  static API_URL = apiURL
+
+  /* news */
   static NEWS_RECENT = `${apiURL}/api/news/recent`
   static NEWS_ALL = `${apiURL}/api/news/all`
   static NEWS_ID = `${apiURL}/api/news`
+
+  /* user */
+  static USER_SKILL = `${apiURL}/api/user/skill`
+  static USER_SEARCH = `${apiURL}/api/user/search`
 }
